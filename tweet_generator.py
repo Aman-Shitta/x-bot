aa13c03ddeb385f50662f80dc6f1099786bf805a
aman <amanshitta18@gmail.com>
2025-03-21 12:57:23 +0530

    Create a blueprint for AI based tweet generator.

---
+++ tweet_generator.py
@@ -0,0 +1,10 @@
+from config import AIConfig
+
+class TweetGenerator:
+    def __init__(self, config: AIConfig):
+        self.config = config
+        
+    def generate_tweet(self, topic: str = "gotta think of one soon!") -> str:
+        pass
+        
+        return ""
